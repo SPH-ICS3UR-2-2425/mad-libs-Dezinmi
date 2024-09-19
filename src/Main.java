2ab006598124e0574e690d57335daa28e96dcb87
Dezinmi <nurihan.idris1@stu.ocsb.ca>
2024-09-19 13:20:09 -0400

    almost i swear

--- src/Main.java
+++ src/Main.java
@@ -19,15 +19,17 @@ public class Main {
 		String girlsName = in.nextLine();
 		System.out.println("Enter a body part");
 		String bodyPart = in.nextLine();
-		System.out.println("Enter a number");
+		System.out.println("Enter a number between 2 and 4");
 		int score = in.nextInt();
 		System.out.println("Enter a decimal number between 1 and 10");
 		double decNumber = in.nextDouble();
+		System.out.println("Enter a type of houshold");
+		String typeOfHousehold = in.nextLine();
 		
 		
 		
 		//STORY
-		System.out.println("One day during "+sport+" practice, "+girlsName+" broke her "+bodyPart+" when she was trying to shoot "+score+" in the net while there was "+decNumber+" minutes left in the game. Then she went to ");
+		System.out.println("One day during "+sport+" practice, "+girlsName+" broke her "+bodyPart+" when she was trying to shoot "+score+" points in the net while there was "+decNumber+" minutes left in the game. She didn't know that though. Then she went to her "+typeOfHousehold" and told her parent's what has hapopened today");
 		
 
 	}
